0ff121e43ca1bc752d10fca49dd49a6b5fa7e3f3
Bryan Neumann <Bryan Neumann>
2017-06-20 07:59:48 -0500

    update local file

--- app.js
+++ app.js
@@ -58,10 +58,8 @@ function mainMenu(person, people){
 function searchByName(people){
   var firstName = promptFor("What is the person's first name?", chars);
   var lastName = promptFor("What is the person's last name?", chars);
-
   // TODO: find the person using the name they entered
 
-  return person;
 
 
 }
