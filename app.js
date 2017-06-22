f7f0652690af4d11be8fffa6243fc02c73df9f24
John Lor <j_lor@hotmail.com>
2017-06-21 21:56:19 -0500

    update search trait function

--- app.js
+++ app.js
@@ -14,24 +14,38 @@ function app(people){
     break;
     case 'no':
     // TODO: search by traits
-		var searchTraitType = prompt("Which option would you like to search by? \nGender\nDate of Birth\nHeight\nWeight\nOccupation\n\nType the option you want or 'restart' or 'quit'").toLowerCase();
-		// var info = searchByTrait();
+		// var person = searchByTrait(people);
+		// console.log(person);
+		// console.log(person.firstName);		
+		// mainMenu(person, people);
+		
+		var searchTraitType = promptFor("Which option would you like to search by? \nGender\nDate of Birth\nHeight\nWeight\nEye Color\nOccupation\n\nType the option you want or 'restart' or 'quit'", chars).toLowerCase();
+
 		switch(searchTraitType){
 			case 'gender':
 				var person = searchByGender(people);
-				mainMenu(person, people);
+				console.log(person);
 			break;
+			case 'dob':
 			case 'date of birth':
-			
+				var person = searchByDob(people);
+				console.log(person);
 			break;
 			case 'height':
-			
+				var person = searchByHeight(people);
+				console.log(person);
 			break;
 			case 'weight':
-			
+				var person = searchByWeight(people);
+				console.log(person);
+			break;
+			case 'eye color':
+				var person = searchByEyeColor(people);
+				console.log(person);
 			break;
 			case 'occupation':
-			
+				var person = searchByOccupation(people);
+				console.log(person);
 			break;
 			case "restart":
 				app(people); // restart
@@ -91,14 +105,15 @@ function mainMenu(person, people){
 function searchByName(people){
   var firstName = promptFor("What is the person's first name?", chars);
   var lastName = promptFor("What is the person's last name?", chars);
- 	if(/[0-9]/.test(firstName)){
-     	alert("The first name must be alphanumerical");
-}	else if(/[0-9]/.test(lastName)){
-    	alert("The last name must be alphanumerical");
-}
-  var person;
+ 	// if(/[0-9]/.test(firstName)){
+     	// alert("The first name must be alphanumerical");
+// }	else if(/[0-9]/.test(lastName)){
+    	// alert("The last name must be alphanumerical");
+// }
+
   // TODO: find the person using the name they entered
- 	for (var x = 0; x < people.length; x++) {
+ 	var person = [];
+	for (var x = 0; x < people.length; x++) {
 		person = people[x];
 			if ((person.firstName.toLowerCase() === firstName.toLowerCase()) && (person.lastName.toLowerCase() === lastName.toLowerCase())) {
             return person;
@@ -154,7 +169,7 @@ function displayDescendants(person,descendants){
 		alert(person.firstName+ " " + person.lastName+" has no descendants.");
 	}
 	else {
-		alert(person.firstName+ " " + person.lastName+"'s descendants are: \n"+descendants);
+		alert(person.firstName+ " " + person.lastName+"'s descendants are: \n\n"+descendants.join("\n"));
 	}
 }
 
@@ -177,34 +192,87 @@ function chars(input){
   return true; // default validation only
 }
 
-function searchByTrait () {
+function searchByTrait (people) {
+	var gender = searchByGender(people);
+	var dob = searchByDob(people);
+	console.log("dob ",dob);
 	
+	var newArray = people.filter(function(person) {
+		return (person.dob === dob);});
+
+	console.log("new array ",newArray[0]);
+	newArray = newArray[0];
+	return newArray;
 }
 
 function searchByGender(people) {
-	var gender = prompt("What is the person's gender?", chars);
+	var gender = promptFor("What is the person's gender?",chars);
+	var person;
+	
+	var newArray = people.filter(function(person) {
+		return (person.gender === gender);});
+	console.log(newArray);
+	alert("Gender: "+gender+("\n")+newArray.map(function(person) {return person.firstName + " " + person.lastName}).join("\n"));
 
+	return newArray;
 }
 
-// function searchByDob() {
-// 	var dob = promptFor("What is the person's date of birth?", chars);
-// }
+function searchByDob(people) {
+	var dob = prompt("What is the person's date of birth?\n\nThe date format should be : mm/dd/yyyy");
+	
+	var newArray = people.filter(function(person) {
+		return (person.dob === dob);});
+	console.log("first ",newArray);
+	alert("Date of Birth: "+dob+("\n")+newArray.map(function(person) {return person.firstName + " " + person.lastName}).join("\n"));
 
-// function searchByHeight() {
-// 	var height = promptFor("What is the person's height?", chars);
-// }
+	var newDob = newArray.map(function(person) {return person.dob});
+	
+	return newDob.join();
+}
 
-// function searchByWeight() {
-// 	var weight = promptFor("What is the person's weight?", chars);
-// }
+function searchByHeight(people) {
+	var height = promptFor("What is the person's height?", chars);
+		height = parseInt(height);
+	var newArray = people.filter(function(person) {
+		return (person.height === height);});
+	console.log(newArray);
+	alert("Height: "+height+("\n")+newArray.map(function(person) {return person.firstName + " " + person.lastName}).join("\n"));
 
-// function searchByEyeColor() {
-// 	var eyeColor = promptFor("What is the person's eye color?", chars);
-// }
+	return newArray;
+}
 
-// function searcyByOccupation() {
-// 	var occupation = promptFor("What is the person's occupation?", chars);
-// }
+function searchByWeight(people) {
+	var weight = promptFor("What is the person's weight?", chars);
+		weight = parseInt(weight);
+	var newArray = people.filter(function(person) {
+	return (person.weight === weight);});
+	console.log(newArray);
+	alert("Weight: "+weight+("\n")+newArray.map(function(person) {return person.firstName + " " + person.lastName}).join("\n"));
+
+	return newArray;
+}
+
+function searchByEyeColor(people) {
+	var eyeColor = promptFor("What is the person's eye color?", chars);
+	
+	var newArray = people.filter(function(person) {
+	return (person.eyeColor === eyeColor);});
+	console.log(newArray);
+	alert("Eye Color: "+eyeColor+("\n")+newArray.map(function(person) {return person.firstName + " " + person.lastName}).join("\n"));
+
+	return newArray;
+}
+
+function searchByOccupation(people) {
+	var occupation = promptFor("What is the person's occupation?", chars);
+	
+	var newArray = people.filter(function(person) {
+	return (person.occupation === occupation);});
+	console.log(newArray);
+	alert("Occupation: "+occupation+("\n")+newArray.map(function(person) {return person.firstName + " " + person.lastName}).join("\n"));
+
+	return newArray;
+}
 
 
 function findDescendants(person, people, descendants, x=0) {
